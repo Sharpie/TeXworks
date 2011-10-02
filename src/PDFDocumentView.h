8c1b7edb3a344244ba1d8a048b2a8a1c8c5a5ee4
Charlie Sharpsteen <source@sharpsteen.net>
2011-10-02 16:11:55 -0700

    Generalize view zooming with a zoomBy function
    Added the `zoomBy` slot to `PDFDocumentView` which takes a `qreal` argument
    representing a zoom factor and applies it to the view.

--- src/PDFDocumentView.h
+++ src/PDFDocumentView.h
@@ -63,6 +63,7 @@ public slots:
   void setMagnifierShape(const MagnifierShape shape);
   void setMagnifierSize(const int size);
 
+  void zoomBy(qreal zoomFactor);
   void zoomIn();
   void zoomOut();
 
